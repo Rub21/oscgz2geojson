11237c9a789f6abeb7502982063087a58c170b2d
Aaron Lidman <aaronlidman@gmail.com>
2014-04-08 17:16:39 -0400

    bbox limiting

--- osc2geojson.py
+++ osc2geojson.py
@@ -2,11 +2,16 @@ from sys import argv, exit
 import xml.parsers.expat
 import json
 
+minlat = 40.68636570811299
+minlon = -74.06227111816406
+maxlat = 40.885486181691945
+maxlon = -73.84117126464844
+
 create = False
 refNodes = {}
 nodes = []
 ways = []
-f = open('nodes.datamap', 'w')
+f = open('nodes.geojson', 'w')
 
 def start_element(name, attrs):
     global create
@@ -15,18 +20,19 @@ def start_element(name, attrs):
     if name == 'create':
         create = True
     if create and name == 'node':
-        nodes.append({
-          "type": "Feature",
-          "properties": {},
-          "geometry": {
-            "type": "Point",
-            "coordinates": [attrs['lon'], attrs['lat']]
-          }
-        });
-        refNodes[attrs['id']] = {
-            'lat': attrs['lat'],
-            'lon': attrs['lon']
-        }
+        if float(attrs['lat']) >= minlat and float(attrs['lat']) <= maxlat and float(attrs['lon']) >= minlon and float(attrs['lon']) <= maxlon:
+            nodes.append({
+              "type": "Feature",
+              "properties": {},
+              "geometry": {
+                "type": "Point",
+                "coordinates": [float(attrs['lon']), float(attrs['lat'])]
+              }
+            });
+            refNodes[attrs['id']] = {
+                'lat': attrs['lat'],
+                'lon': attrs['lon']
+            }
 
 def end_element(name):
     global create
@@ -39,7 +45,7 @@ def end_element(name):
         for node in nodes:
             outNodes += json.dumps(node) + ','
 
-        outNodes += ']}'
+        outNodes = outNodes[:-1] + ']}'
         f.write(outNodes)
 
 p = xml.parsers.expat.ParserCreate()
